5d2c5fc4126de54804984101145eee8fba0c8e03
Nina Weiler <ninaweiler3@gmail.com>
2021-04-29 15:17:04 +0300

    unicafe final step

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,84 +1,132 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>
-      {props.text}
-    </button>
-  )
-}
-
-const Reviews = (props) => {
-  return (
-    <div>
-      <p>{props.text} {props.state}</p>
-    </div>
-  )
-}
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
 
 const ReviewsAverage = (props) => {
-  if(props.state.length > 0) {
-    let average = props.state.reduce((a, b) => a + b, 0) / props.state.length
+  if (props.value.length > 0) {
+    let average = props.value.reduce((a, b) => a + b, 0) / props.value.length;
     return (
-      <p>{props.text} {average}</p>
-    )
+      <table>
+        <tbody>
+          <tr>
+            <td width="60px">
+            {props.text}
+            </td>
+            <td>
+            {average}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      
+    );
   }
-  return <p>{props.text} 0</p>
-}
-
+  return <p>{props.text} 0</p>;
+};
 
 const PositiveReviews = (props) => {
-  if(props.state.length > 0){
-  let positives = props.state.filter(value => value === 1)
-  let percentage = (positives.length * 100)/props.state.length
+  if (props.value.length > 0) {
+    let positives = props.value.filter((value) => value === 1);
+    let percentage = (positives.length * 100) / props.value.length;
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td width="60px">
+            {props.text}
+            </td>
+            <td>
+            {percentage}%
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+  return <p>{props.text} 0%</p>;
+};
+
+const Statistic = ({text, value}) => {
   return (
-    <p>{props.text} {percentage}%</p>
-  )}
-  return <p>{props.text} 0%</p>
+    <table>
+      <tbody>
+     <tr>
+      <td width="60px">
+      {text}
+      </td>
+      <td>
+      {value}
+      </td>
+      </tr>  
+      </tbody> 
+    </table>
+   
+  );
 }
 
+const Statistics = (props) => {
+  if (props.allReviews.length > 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <Statistic value={props.good} text="good" />
+        <Statistic value={props.neutral} text="neutral" />
+        <Statistic value={props.bad} text="bad" />
+        <Statistic value={props.allReviews.length} text="all" />
+        <ReviewsAverage value={props.allReviews} text="average" />
+        <PositiveReviews value={props.allReviews} text="positive" />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+    </div>
+  );
+};
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allReviews, setAllReviews] = useState([])
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allReviews, setAllReviews] = useState([]);
 
   const goodReview = () => {
-    setAllReviews(allReviews.concat(1))
-    setGood(good + 1)
-    console.log('added good', allReviews)
-  }
+    setAllReviews(allReviews.concat(1));
+    setGood(good + 1);
+  };
 
   const neutralReview = () => {
-    setAllReviews(allReviews.concat(0))
-    setNeutral(neutral + 1)
-    console.log('added neutral', allReviews)
-}
+    setAllReviews(allReviews.concat(0));
+    setNeutral(neutral + 1);
+    
+  };
   const badReview = () => {
-    setAllReviews(allReviews.concat(-1))  
+    setAllReviews(allReviews.concat(-1));
     setBad(bad + 1)
-    console.log('added bad', allReviews)
- }
+  };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={goodReview} text="good"/>
-      <Button handleClick={neutralReview} text="neutral"/>
-      <Button handleClick={badReview} text="bad"/>
-      <h1>statistics</h1>
-      <Reviews state={good} text="good"/>
-      <Reviews state={neutral} text="neutral"/>
-      <Reviews state={bad} text="bad"/>
-      <Reviews state={allReviews.length} text="all"/>
-      <ReviewsAverage state={allReviews} text="average"/>
-      <PositiveReviews state={allReviews} text="positive"/>
+      <Button handleClick={goodReview} text="good" />
+      <Button handleClick={neutralReview} text="neutral" />
+      <Button handleClick={badReview} text="bad" />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        allReviews={allReviews}
+      />
+
       <p></p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
