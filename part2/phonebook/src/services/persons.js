08f8dfe22209ad34f45fb307bc357c464f1d4eb6
Nina Weiler <ninaweiler3@gmail.com>
2021-05-17 11:31:04 +0300

    Phonebook full stack

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
@@ -30,4 +30,8 @@ const update = (id, newObject) => {
 } 
 
 
-export default { getAll, create, update, remove }
+const personService = {getAll, create, update, remove}
+
+
+
+export default personService;
