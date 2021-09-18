521a48b044cec5fc857e6b808a464ae0fcaf368e
Nina Weiler <ninaweiler3@gmail.com>
2021-09-18 13:15:18 +0300

    5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -5,6 +5,12 @@ describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     // create user
+    const user = {
+      name: 'Callie Jacobs',
+      username: 'callie',
+      password: 'brandon'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -12,11 +18,31 @@ describe('Blog app', function () {
     cy.contains('Sign in')
     cy.contains('login').click()
   })
-  /*
+
   describe('Login', function() {
     it('succeeds with correct credentials', function() {
+      cy.contains('Sign in').click()
+      cy.get('#username').type('callie')
+      cy.get('#password').type('brandon')
+      cy.get('#login-button').click()
+
+      cy.contains('callie logged in')
     })
-  }) */
+
+    it.only('fails with wrong credentials', function() {
+      cy.contains('Sign in').click()
+      cy.get('#username').type('callie')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'callie logged in')
+    })
+  })
 
 })
 
