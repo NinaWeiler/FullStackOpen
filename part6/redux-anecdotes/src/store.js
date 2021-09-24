9137707d518d3993dd03c667f4ad2ac3415c0408
Nina Weiler <ninaweiler3@gmail.com>
2021-09-24 15:48:51 +0300

    6.9

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,8 @@
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+const store = createStore(anecdoteReducer, composeWithDevTools())
+
+export default store;
